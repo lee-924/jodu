45e7dec7413aabfb2d8b7c1b45f9b63e62417606
lee <ahumada14344@gmail.com>
2025-08-14 18:41:29 +0300

    first game completed (yapping left)

--- js/tapGame.js
+++ js/tapGame.js
@@ -1,42 +1,187 @@
 let counter = document.querySelector('.counter')
 let hiddenDiv = document.querySelector('.clickDiv')
-
-
-let count = 99;
+let barYap = document.querySelector('.barYap')
+let counterr = document.querySelector('.count')
+let vari;
+let hundreds = 0
+let count = 0;
 console.log(count)
 let bar = document.querySelector('.bar')
+click(count,hundreds)
 
-funtion click() {
-counter.addEventListener('pointerdown', () => {
+
+
+
+
+
+function click(count,hundreds) {
+if (count) {
+bar.style.display = 'block'
+bar.style.height = `${count}%`
+bar.style.width = `${count}%`
+counterr.innerHTML = (hundreds*100 + count);
+counter.style.background = '#333'
+barYap.innerHTML = 'Tap'
+barYap.style.color = 'white'
+counter.style.color= 'white'}
+
+
+vari = function() {
+    console.log('log')
+    bar.classList.remove("pulse"); 
+    void bar.offsetWidth; 
+    bar.classList.add("pulse");
     count += 1
     bar.style.height = `${count}%`
-    bar.style.width = `${count}%`
-    let counterr = document.querySelector('.count');
-    counterr.innerHTML = count; 
+    bar.style.width = `${count}%`;
+    counterr.innerHTML = (hundreds*100 + count);
 
-if (count === 100) {
-    
-    counter.style.background = 'white'
-    counter.innerHTML = 'YOU DID IT!'
-    counter.style.color= 'black'
-    count = 0;
-    hundred()
-   
-}
-}) }
 
+    if (hundreds*100 + count === 100) {
+        hundred() }
+    else if (hundreds*100 + count === 200) {
+        two()
+    }
+
+    else if (hundreds*100 + count === 300) {
+        three()
+    }
+
+    }
+
+counter.addEventListener('pointerdown', vari)
+colorVerify()
+
+ }
+
+let text = document.querySelector('.text')
 function hundred() {
-    console.log('YOU DID IT')
+    hundreds = 1
+    colorVerify()
     hiddenDiv.style.display = 'flex'
     hiddenDiv.style.animationPlayState = 'running'
+    text.innerHTML= `
+My Love 
+
+I am always here for you 
+no matter what you go through in life`
+
+let ind = document.querySelector('.firstLine')   
+ind.style.display = 'flex'
+    counter.removeEventListener('pointerdown',vari)
+
+    count = 0
+    click(count,hundreds) }
+
+
+
+let jumpTwo = document.querySelector('.two')
+let jumptwoFunc;
+jumptwoFunc = function() {
+    console.log('lol')
+    counter.removeEventListener('pointerdown',vari)
+    hundreds = 1;
+    count = 99;
+    click(count,hundreds)}
+jumpTwo.addEventListener('click', jumptwoFunc)
+
+
+function two() {
+    hundreds = 2
+    colorVerify()
+    hiddenDiv.style.display = 'flex'
+    hiddenDiv.style.animationPlayState = 'running'
+    text.innerHTML= `
+My Love 
+
+this is the second one`
+
+let inds = document.querySelector('.secLine')   
+inds.style.display = 'flex'
+counter.removeEventListener('pointerdown',vari)
+count = 0
+click(count,hundreds)
+}
+
+
+
+function three() {
+    hundreds = 3
+    colorVerify()
+    hiddenDiv.style.display = 'flex'
+    hiddenDiv.style.animationPlayState = 'running'
+    text.innerHTML= `
+GAME OVER
+
+this is the third and FINAL MESSAGE.`
+
+
+
+let inds = document.querySelector('.secLine') 
+inds.innerHTML = `
+<button class="sec b">Jump to 200</button>
+<button class="three b">Jump to 300</button>`  ;
+
+let jSec = document.querySelector('.sec')
+jSec.addEventListener('click',jumptwoFunc)
+
+let thirdJump = document.querySelector('.three')
+
+thirdJump.addEventListener('click', () => {
+    hundreds = 2;
+    counter.removeEventListener('pointerdown',vari)
+    count = 99;
+    click(count,hundreds)
+})
+inds.style.display = 'flex'
+    counter.removeEventListener('pointerdown',vari)
+    count = 0
+    click(count,hundreds)
+}
+
+function colorVerify() {
+     if (hundreds === 0) {
+        bar.style.backgroundColor = '#16db65'
+    }
+    else  if (hundreds === 1) {
+        bar.style.backgroundColor = '#0d00a4'
+        hiddenDiv.style.backgroundColor = '#16db65'
+    }
+    else  if (hundreds === 2) {
+        bar.style.backgroundColor = '#f15946'
+        hiddenDiv.style.backgroundColor = '#0d00a4'
+    }
+    else if (hundreds === 3) {
+        bar.style.backgroundColor = '#e01a4f'
+        hiddenDiv.style.backgroundColor = '#f15946'
+    }
 }
 
 let cancel = document.querySelector('.cancel')
 cancel.addEventListener('click', () => {
     hiddenDiv.style.display = 'none'
+    hiddenDiv.style.animationPlayState = 'paused'
 })
 
 let resetCount = document.querySelector('.resetCount')
 resetCount.addEventListener('click', () => {
-    count = 0; 
+    hundreds = 0
+    console.log('clicked')
+    counter.removeEventListener('pointerdown',vari)
+    count = 1;
+    click(count,hundreds)
+})
+
+let jumpHundred = document.querySelector('.hun')
+jumpHundred.addEventListener('click', () => {
+    hundreds = 0;
+    counter.removeEventListener('pointerdown',vari)
+    count = 99;
+    click(count,hundreds)
+})
+
+
+let home = document.querySelector('.home')
+home.addEventListener('click', () => {
+    window.location.href = 'index.html'
 })
